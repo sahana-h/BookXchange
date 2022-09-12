ff5df9a5cbdb3c646a9319eff8896a6d51c8c86a
Sahana Hariharan <sahanahariharan@gmail.com>
2022-09-12 01:41:40 -0700

    Final fix for match

--- initial/src/main/java/com/example/springboot/MatchWantsAndHaves.java
+++ initial/src/main/java/com/example/springboot/MatchWantsAndHaves.java
@@ -17,13 +17,23 @@ public class MatchWantsAndHaves {
         this.userid = userid;
     }
 
-    public void DoMatching() {
+    public List<UserISBNTuple> DoMatchingForMyWants() {
         // Connect to Database
         Set<Long> usersWant = getUsersWant();
-        List<Long> userIds = getUsersWhoHaveBooks(usersWant);
-        for (Long id : userIds) {
-            System.out.println("UserId:" + id + " has the book I need");
-        }
+        // List<Long> userIds = getUsersWhoHaveBooks(usersWant);
+        List<UserISBNTuple> matchingUsers = getUsersWhoHaveBooks(usersWant);
+
+
+        // ArrayList<UserISBNTuple> matchingUsers = new ArrayList<UserISBNTuple>();
+
+        // for (Long id : userIds) {
+        //     System.out.println("UserId:" + id + " has the book I need");
+
+        //     UserISBNTuple tup = new UserISBNTuple(id, );
+        //     matchingUsers.add(id);
+        // }
+
+        return matchingUsers;
     }
 
     private Set<Long> getUsersWant() {
@@ -42,23 +52,68 @@ public class MatchWantsAndHaves {
         return wantIsbns;
     }
 
-    private List<Long> getUsersWhoHaveBooks(Set<Long> isbns) {
+    // private Long getUserZip() {
+    //     Connection connection = Application.getConnection();
+        
+    //     try {
+    //         Statement stmt = connection.createStatement();
+    //         ResultSet result = stmt.executeQuery(
+    //                 "select * from user_info where userid=" + userid + " AND active= 1");
+    //         while (result.next()) {
+    //             wantIsbns.add(result.getLong("isbn"));
+    //         }
+    //     } catch (SQLException e) {
+    //         System.out.println(e);
+    //     }
+    //     return wantIsbns;
+    // }
+
+    private List<UserISBNTuple> getUsersWhoHaveBooks(Set<Long> isbns) {
         Connection connection = Application.getConnection();
-        List<Long> userIds = new ArrayList<>();
+        // List<Long> userIds = new ArrayList<>();
+
+        List<UserISBNTuple> tup = new ArrayList<UserISBNTuple>();
+
         try {
             Statement stmt = connection.createStatement();
             ResultSet result = stmt.executeQuery(
-                    "select * from books_available where active= 1");
-            while (result.next()) {
-                Long isbn = result.getLong("userid");
+                    "select * from books_available where active= 1 and userid<>" + userid);
+            while (result != null && result.next()) {
+                Long neighboruserid = result.getLong("userid");
+                Long isbn = result.getLong("isbn");
                 if (isbns.contains(isbn)) {
-                    userIds.add(isbn);
+
+                    // Someone has the book that I need
+                    // Check if this person has my same zip code
+                    String emailid = getEmail(neighboruserid);
+
+                    UserISBNTuple t = new UserISBNTuple(neighboruserid, emailid, isbn);
+                    tup.add(t);
                 }
             }
         } catch (SQLException e) {
             System.out.println(e);
         }
-        return userIds;
+        return tup;
+    }
+
+    String getEmail(Long userid) {
+        Connection connection = Application.getConnection();
+        Statement stmt;
+
+        try {
+            stmt = connection.createStatement();
+            ResultSet result = stmt
+                    .executeQuery("select emailid from user_info where userid=" + userid);
+            while (result != null && result.next()) {
+                String emailid = result.getString("emailid");
+                return emailid;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+        }
+        return null;
     }
 
 }
